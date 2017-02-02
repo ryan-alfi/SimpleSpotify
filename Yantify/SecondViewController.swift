a35f2ca8190f362cc6347db2c8ab04cc22035d3a
Ryan <real.rezz@gmail.com>
2017-02-02 13:19:24 +0700

    Initial Commit

---
+++ Yantify/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Yantify
+//
+//  Created by iMac on 2/2/17.
+//  Copyright © 2017 Ari Fajrianda Alfi. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
